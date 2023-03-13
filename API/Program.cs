04d50f050953671f674ff0fae6bbade206ba2c3e
prajwalpinto <ppinto@rein.ai>
2023-03-13 12:36:17 -0300

    delete controller and cleanup

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using MediatR;
-using Application;
-using Application.Core;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,21 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000/");
-    });
-});
-
-builder.Services.AddMediatR(typeof(List.Handler));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

902d9da352efc1fea0d7b99232390942679afe52
prajwalpinto <ppinto@rein.ai>
2023-03-16 17:25:08 -0300

    axios cleanup and crud operations

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
@@ -6,8 +6,15 @@ interface Props {
   activities: Activity[];
   selectActivity:(id:string) => void;
   deleteActivity:(id:string) => void;
+  submitting: boolean;
 }
-export default function ActivityList({activities,selectActivity,deleteActivity}:Props){
+export default function ActivityList({activities,selectActivity,deleteActivity,submitting}:Props){
+  const [target,setTarget]=useState('');
+
+  function handleActivityDelete(e:SyntheticEvent<HTMLButtonElement>,id:string) {
+    setTarget(e.currentTarget.name);
+    deleteActivity(id);
+  }
   return(
     <Segment>
       <Item.Group divided>
@@ -21,8 +28,15 @@ export default function ActivityList({activities,selectActivity,deleteActivity}:
               <div>{activity.city}, {activity.venue}</div>
             </Item.Description>
             <Item.Extra>
-              <Button floated="right" content="View" color="blue" onClick={()=>selectActivity(activity.id)} />
-              <Button floated="right" content="Delete" color="red" onClick={()=>deleteActivity(activity.id)} />
+              <Button floated="right" content="View" color="blue" onClick={()=>selectActivity(activity.id)}  />
+              <Button 
+              floated="right" 
+              content="Delete" 
+              color="red" 
+              onClick={(e) => handleActivityDelete(e,activity.id)} 
+              loading={submitting && target===activity.id} 
+              name={activity.id}
+              />
               <Label basic content={activity.category} />
             </Item.Extra>
           </Item.Content>
